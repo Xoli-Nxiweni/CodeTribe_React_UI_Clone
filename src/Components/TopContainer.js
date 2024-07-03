7324e5a84eeeb11cce4c6688206e047ed1f97680
Xoli Nxiweni <xolinxiweni@gmail.com>
2024-07-03 15:42:57 +0200

    adding another container

--- src/Components/TopContainer.js
+++ src/Components/TopContainer.js
@@ -13,17 +13,24 @@ let TopContainer = () =>{
 
             <button>Get's More</button>
         </div>
-        <div className = 'TopRightContainer'>
-            <div className ='TwoLT'>
-            <h1>2x</h1>
-            <p>Less Time</p>
+        <div className='containerWrapper'>
+            <div className = 'TopRightContainer'>
+                <div className ='TwoLT'>
+                <h1>2x</h1>
+                <p>Less Time</p>
+                </div>
+                <div className = 'TwoLT-Para'>Spent on Development- <br/>Ready Design</div>
+                <div className ='ThirtyP'>
+                <h1>30%</h1>
+                <p>Cut by</p>
+                </div>
+                <div className = 'ThirtyP-Para'>Time on new <br /> Design Delivery</div>
             </div>
-            <div className = 'TwoLT-Para'>Spent on Development- <br/>Ready Design</div>
-            <div className ='ThirtyP'>
-            <h1>30%</h1>
-            <p>Cut by</p>
+            <div className = 'TopRightContainer'>
+                <div className='anotherLogo'>
+                <img src='https://img.icons8.com/?size=40&id=212ADDKBpxxA&format=png'/> <h1>Minds</h1>
+                </div>
             </div>
-            <div className = 'ThirtyP-Para'>Time on new <br /> Design Delivery</div>
         </div>
         </div>
     );
