1e9e8807d9adf6e213a6d2a7e763f7d8e318914c
Xoli <xolinxiweni@gmail.com>
2024-07-05 08:43:36 +0200

    05 July 2023

--- src/Components/BottomContainer.js
+++ src/Components/BottomContainer.js
@@ -9,16 +9,26 @@ let BottomContainer = () =>{
                 <div className='anotherLogo2'>
                     <img src='https://img.icons8.com/?size=40&id=212ADDKBpxxA&format=png'/> <h1>Minds</h1>
                 </div>
-                <h3>Want to <br/> know more<br /> about us?</h3>
+                <h3>Want to <br/> know more<br /> 
+                about us?</h3>
                 <div className='belowPart'>
                 <img src= 'https://st4.depositphotos.com/1216158/28904/v/450/depositphotos_289041782-stock-illustration-radiating-radial-circle-design-element.jpg' /> 
                 <button>Try For Free</button>
                 </div>
             </div>
             <div className='contentTwo'>
-                <p>Your creative idea we<br/> will make it easier and <br/> improve your work</p>
+                <div>
+                    <img>
+                        {/* something */}
+                    </img>
+                </div>
+                <div>
+                <p>Your creative idea we<br/> 
+                will make it easier and <br/> 
+                improve your work</p>
                 <button>More</button>
                 <img/>
+                </div>
             </div>
             
         </div>
