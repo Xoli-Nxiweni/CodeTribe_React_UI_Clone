076ed4bfaf81b5561f359044a8dd0d7d4c6ac878
Xoli <xolinxiweni@gmail.com>
2024-08-29 16:02:51 +0200

    Fixed responsiveness of my UI challenge

--- src/App.js
+++ src/App.js
@@ -4,21 +4,31 @@ import TopContainer from './Components/TopContainer';
 import BottomContainer from './Components/BottomContainer';
 import RightContainers from './Components/RightContainers';
 
-function App() {
-  return (
-    <div className="Wrapper">
-      <NavigationBar />
-        <div className='Containers'>
-          <div className='leftContainers'>
-            <TopContainer />
-            <BottomContainer />
-          </div>
-          <div className='rightContainer'>
-            <RightContainers />
-          </div>
-        </div>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="Wrapper">
+//       <NavigationBar />
+//         <div className='Containers'>
+//           <div className='leftContainers'>
+//             <TopContainer />
+//             <BottomContainer />
+//           </div>
+//           <div className='rightContainer'>
+//             <RightContainers />
+//           </div>
+//         </div>
+//     </div>
+//   );
+// }
 
+let App = () =>{
+  return(
+    <div className='Wrapper'>
+      <div className='NavBar'><NavigationBar/></div>
+      <div className='TopCont'><TopContainer/></div>
+      <div className='BottomCont'><BottomContainer/></div>
+      <div className='RightCont'><RightContainers/></div>
+    </div>
+  )
+}
 export default App;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,23 +1,43 @@
-import './NavBar.css'
+import React, { useState } from 'react';
+import './NavBar.css';
 
-let NavigationBar = () =>{
-    return(
-        <div className = 'NavBar'>
-            <div className = "logo">
-            <img src='https://img.icons8.com/?size=40&id=212ADDKBpxxA&format=png'/> <h1>Minds</h1>
+const NavigationBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className={`NavBar ${isOpen ? 'active' : ''}`}>
+            <div className="logo">
+                <img src='https://img.icons8.com/?size=40&id=212ADDKBpxxA&format=png' alt='logo' />
+                <h1>Minds</h1>
+            </div>
+
+            <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
 
             <ul>
                 <li>
-                    <a href='#'>Home</a>
-                    <a href='#'>About</a>
-                    <a href='#'>Blog</a>
-                    <a href='#'>Jobs</a>
+                    <a href='#' onClick={()=> console.log('Home Button Clicked')
+                    }>Home</a>
+                    <a href='#' onClick={()=> console.log('About Button Clicked')
+                    }>About</a>
+                    <a href='#' onClick={()=> console.log('Blog Button Clicked')
+                    }>Blog</a>
+                    <a href='#' onClick={()=> console.log('Jobs Button Clicked')
+                    }>Jobs</a>
                 </li>
             </ul>
 
-            <button className='btn1'>Your Account</button>
+            <button className='btn1' onClick={()=> console.log('Your Account button clicked')
+            }>Your Account</button>
         </div>
-    )
-}
+    );
+};
+
 export default NavigationBar;
